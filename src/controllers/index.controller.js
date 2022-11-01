d503a0b64760c031b03a029e64a10b565e379ff0
Daniel Cueto <danielcuetorrico@gmail.com>
2022-11-01 14:47:08 -0400

    corrigiendo la prueba ping

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,7 +1,5 @@
 import { pool } from "../db.js";
 
 export const ping = async (req, res) => {
-
-  const result = await pool.query("select* from consultorio;");
-  res.json(result[0]);
+  res.send("pong");
 };

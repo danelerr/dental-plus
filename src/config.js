c21fb32da2d246027e19dea8c53502fec2940c2e
fabio <fabioarredondo44@gmail.com>
2022-12-13 20:45:14 -0400

    Actualizando

---
+++ src/config.js
@@ -0,0 +1,11 @@
+import { config } from 'dotenv';
+
+config();
+
+export const PORT = 3000;
+export const DB_USER =  'root';
+export const DB_PASSWORD = 'hj5xorYiEJy9EcOXsxyI';
+export const DB_HOST = 'containers-us-west-43.railway.app';
+export const DB_DATABASE = 'railway';
+export const DB_PORT = 6315;
+ 

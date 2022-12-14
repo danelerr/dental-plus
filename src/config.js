41b85cec6247bda2e84f385e3947309093991ca6
Daniel Cueto <danielcuetorrico@gmail.com>
2022-12-14 15:56:49 -0400

    correccion en el archivo config

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ import { config } from 'dotenv';
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const DB_USER = 'root';
-export const DB_PASSWORD = 'hj5xorYiEJy9EcOXsxyI';
-export const DB_HOST = 'containers-us-west-43.railway.app';
-export const DB_DATABASE = 'railway';
-export const DB_PORT = 6315;
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 123777;
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_DATABASE = process.env.DB_DATABASE || 'BDDentalPlus';
+export const DB_PORT = process.env.DB_PORT || 3306;
 
  

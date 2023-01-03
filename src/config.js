bf319409cc3f327077e7ad81cc09ff7d021812d3
Daniel Cueto <danielcuetorrico@gmail.com>
2023-01-03 09:23:22 -0400

    Agregado el odontograma

--- src/config.js
+++ src/config.js
@@ -8,4 +8,4 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || 123777;
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_DATABASE = process.env.DB_DATABASE || 'BDDentalPlus';
 export const DB_PORT = process.env.DB_PORT || 3306;
- 
+
